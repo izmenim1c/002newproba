02d794fb48a46377ee07ca2655223e2bffabdd13
izmenim1c <izmenim1c.ru@ya.ru>
2022-01-27 21:24:24 +0300

    Создание первого фрагмента для списка сообщений

---
+++ app/src/main/java/com/example/a002newproba/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a002newproba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
