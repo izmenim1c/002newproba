57ae8acbf007e5204bf62b6996f0420b5091e4b2
izmenim1c <izmenim1c.ru@ya.ru>
2022-02-03 20:05:46 +0300

    Добавил обработчики нажатия и вывод описания во втором фрагменте в портретной и ландшафтной ориентации

--- app/src/main/java/com/example/a002newproba/ListMessegesFragment.java
+++ app/src/main/java/com/example/a002newproba/ListMessegesFragment.java
@@ -1,5 +1,6 @@
 package com.example.a002newproba;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,24 +19,25 @@ import android.widget.TextView;
  * create an instance of this fragment.
  */
 public class ListMessegesFragment extends Fragment {
-
+    Messege currentmassege;
+    boolean isLandScape;
 
     public static ListMessegesFragment newInstance() {
-           return new ListMessegesFragment();
+        return new ListMessegesFragment();
     }
 
-    /** @Override
+     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-    }*/
+         isLandScape = getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_list_messeges, container, false);
-       // TextView textView = rootView.findViewById(R.id.fragment_container);
+
 
         return rootView;
     }
@@ -49,12 +51,50 @@ public class ListMessegesFragment extends Fragment {
     private void initList(View view) {
         LinearLayout layoutView = (LinearLayout) view;
         String[] messages = getResources().getStringArray(R.array.messages);
+        int i=0;
         for (String message : messages) {
             TextView tv = new TextView(getContext());
             tv.setText(message);
             tv.setTextSize(30);
             layoutView.addView(tv);
+            int finalI = i++;
+            tv.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ListOnClick(finalI);
+                }
+            });
         }
     }
+
+    private void ListOnClick(int index) {
+        currentmassege = new Messege(index,"заглушка "+index);
+        if (isLandScape) {
+            showReadMessegeLand();
+        }else{ // port
+            showReadMessegePort();
+        }
+
+    }
+
+    private void showReadMessegePort() {
+        requireActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container,ReadMessegeFragment.newInstance(currentmassege))
+                .addToBackStack("")
+                .commit();
+
+    }
+
+    private void showReadMessegeLand() {
+        requireActivity()
+                .getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container_right,ReadMessegeFragment.newInstance(currentmassege))
+                .commit();
+
+    }
+
 
 }
